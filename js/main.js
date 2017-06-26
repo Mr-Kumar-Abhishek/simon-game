faabc9b60f7169a3dccf25f4cbefd34ba9f96d34
Abhishek Kumar <mr.kumar.abhishek@email.com>
2017-06-26 07:50:44 +0530

    turn count screen on and of with switch

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ $("#power-switch").click(function (){
 	if (gameSwitch == false ){
 		
 		$(this).addClass("switch-on");
+		$("#counting").addClass("counting-on");
 		gameSwitch = true;
 	}else {
 		$(this).removeClass("switch-on");
+		$("#counting").removeClass("counting-on");
 		gameSwitch =false;
 	}
 });
